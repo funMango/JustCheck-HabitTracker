b4150769a8ac62d51a3ef19e3158d0c14274d836
funMango <bluemango608@gmail.com>
2025-02-26 17:44:30 +0900

    first commit

---
+++ JustCheck-HabitTracker/JustCheck_HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustCheck_HabitTrackerApp.swift
+//  JustCheck-HabitTracker
+//
+//  Created by 이민호 on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct JustCheck_HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustCheck-HabitTrackerTests/JustCheck_HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustCheck_HabitTrackerTests.swift
+//  JustCheck-HabitTrackerTests
+//
+//  Created by 이민호 on 2/26/25.
+//
+
+import Testing
+@testable import JustCheck_HabitTracker
+
+struct JustCheck_HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
